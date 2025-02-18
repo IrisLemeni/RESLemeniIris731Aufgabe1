c626f64f6d058b1c4b7cc83706e02117ef1f49dc
Iris <lemeniriis@yahoo.ro>
2025-02-18 11:09:47 +0200

    Write File

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,13 +10,25 @@ public class Main {
         Probleme probleme = new Probleme();
         Scanner scanner = new Scanner(System.in);
         List<Ereignis> ereignisList = new ArrayList<>();
+        HashMap<Stufe, Integer> stufePoints= new HashMap<>();
+
+        for (Stufe stufe : Stufe.values()) {
+            stufePoints.put(stufe, 0);
+        }
+
+
         JsonReader.readFromJson("ninja_events.json", ereignisList);
+        for (Ereignis e : ereignisList) {
+            stufePoints.put(e.getStufe(), stufePoints.get(e.getStufe()) + 1);
+        }
 
         System.out.println("Give Kraftpunkte: ");
         double points = Double.valueOf(scanner.nextLine());
         probleme.characterByPoints(points, ereignisList);
 
         probleme.stufeSort(ereignisList);
+
+        probleme.fileWrite(ereignisList, stufePoints);
 
 
     }
--- src/main/java/org/example/Probleme.java
+++ src/main/java/org/example/Probleme.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class Probleme {
@@ -16,6 +19,31 @@ public class Probleme {
                 .filter(e -> e.getStufe() == Stufe.Jonin)
                 .sorted((e1, e2) -> e2.getDate().compareTo(e1.getDate()))
                 .forEach(e -> System.out.println(e.getDate() + ": " + e.getCharacterName() + " - " + e.getDescription()));
+    }
+
+    public void fileWrite(List<Ereignis> ereignisList, HashMap<Stufe, Integer> stufePoints){
+        System.out.println("Ergebnis in gesammtzahl.txt");
+        try (FileWriter fileWriter = new FileWriter("gesammtzahl.txt")) {
+            stufePoints.entrySet().stream()
+                    .sorted((a, b) -> {
+                        int countComparison = Integer.compare(b.getValue(), a.getValue());
+                        if (countComparison == 0) {
+                            // If event counts are the same, sort by house name alphabetically
+                            return a.getKey().name().compareTo(b.getKey().name());
+                        }
+                        return countComparison;
+                    })
+                    .forEach(entry -> {
+                        try {
+                            fileWriter.write(entry.getKey() + "%" + entry.getValue() + "\n");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
